6cb7d1f4f48c5a98de2fd420a67ec17e67b97ee3
Ridwan Lawal <lawal6382@gmail.com>
2024-12-07 14:20:51 +0100

    Nav Design

--- app/_components/Navbar.js
+++ app/_components/Navbar.js
@@ -1,11 +1,45 @@
 import Spinner from "@/app/_components/Spinner";
+import { LogOut, Moon, User } from "lucide-react";
+import Image from "next/image";
 
 function Navbar() {
   return (
     <header>
-      <nav className="border w-full py-4 bg-white">Nav</nav>
+      <nav className="border w-full py-4 bg-white navbar flex justify-end pr-10">
+        <div className="flex items-center gap-8  ">
+          {/* avatar */}
+          <div className="flex items-center gap-3">
+            <div className="relative w-[33px] h-[33px] rounded-full overflow-hidden ">
+              <Image
+                src="/default-user.jpg"
+                alt="avatar"
+                fill
+                className="object-cover"
+              />
+            </div>
+            <p className="text-[14px]">Ridwan</p>
+          </div>
+
+          {/* icons */}
+          <div className="text-color-brand-2  flex items-center gap-3.5 text-sm icons">
+            <button>
+              <User className="size-5" />
+            </button>
+
+            <button>
+              <Moon className="size-5" />
+            </button>
+
+            <button>
+              <LogOut className="size-5" />
+            </button>
+          </div>
+        </div>
+      </nav>
     </header>
   );
 }
 
 export default Navbar;
+
+// git reset to go back to a previous version of your code. (may be soft reset, go back , delete the latest commit, and return the changes to the staging area)
